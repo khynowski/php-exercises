fb47d2ee390d1c6ca31761b5841373da149da2f6
KarolH <karol.hynowski@gmail.com>
2026-01-28 18:43:14 +0100

    Exercise with composer instal and update

--- Week3/index.php
+++ Week3/index.php
@@ -3,8 +3,11 @@
 declare(strict_types=1);
 require __DIR__ . '/vendor/autoload.php';
 
-$greet = new \Hynkar\PhpStarter\Greeting();
+use Hynkar\PhpStarter\Greeting;
+use Carbon\Carbon;
+
+
+$greet = new Greeting();
 echo $greet->sayHello("World");
 
-use Carbon\Carbon;
 printf("Now: %s", Carbon::now()->toDateTimeString());

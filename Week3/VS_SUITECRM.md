f9e3a714689fc95326318a804d20d174e837cace
KarolH <karol.hynowski@gmail.com>
2026-02-26 19:43:05 +0100

    REST controller with --resource

---
+++ Week3/VS_SUITECRM.md
@@ -0,0 +1,6 @@
+## Diff beetween Laravel and SuiteCRM
+1. Laravel to framework czyli narzędzie do tworzenia. SuiteCRM to gotowy produkt.
+2. SuiteCRM często korzysta z czystego SQL. Laravel eloquent ORM
+3. SuiteCRM brak testów. Laravel wbudowany PHPUnit
+4. Customizacja to kod spaghetti często wstrzykiwany gdzieś w widok. Laravel architektura rozwiązań.
+5. Centralny routing w Laravel vs File-based routing w SuiteCRM.
--- Week3/routes/web.php
+++ Week3/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\AccountController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -11,3 +12,4 @@ Route::get('/posts', [PostController::class, 'index']);
 
 Route::get('/posts/create', [PostController::class, 'create']);
 Route::post('/posts', [PostController::class, 'store']);
+Route::resource('accounts', AccountController::class);
